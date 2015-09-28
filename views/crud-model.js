d64a6b1cb2ca03da7b486fe2cb2ac25502c988ab
Linus Miller <lohfu@lohfu.io>
2015-09-28 17:21:33 +0200

    Added crud views from hats/admin

---
+++ views/crud-model.js
@@ -0,0 +1,35 @@
+var app = require('ridge');
+
+module.exports = require('ridge/view').extend({
+	events: {
+		'click button': function(e) {
+			e.stopPropagation();
+		},
+		'click button[data-command="publish"]': 'publish',
+		'click button[data-command="delete"]': 'delete',
+		'click button[data-command="unpublish"]': 'unpublish'
+	},
+
+	delete: function(e) {
+		if(confirm('Are you sure you want to delete the ' + this.model.name + '?')) {
+			this.model.destroy();
+			this.remove();
+		}
+	},
+
+	unpublish: function(e) {
+		this.model.save({ datePublished: null }, { patch: true, wait: true });
+	},
+
+	publish: function(e) {
+		this.model.save({ datePublished: new Date() }, { patch: true, wait: true });
+	},
+
+	initialize: function(options) {
+		this.listenTo(this.model, 'sync', this.render);
+
+		this.listenTo(this.model, 'destroy', this.remove);
+	},
+
+	template: 'admin/models/field',
+});
