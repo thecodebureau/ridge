9cad837737d15c12fa69db8f37fabaeae72acc6f
Linus Miller <linus.miller@thecodebureau.com>
2015-10-06 20:23:14 +0200

    Enabled multiple bindings for model properties.

--- views/crud-form.js
+++ views/crud-form.js
@@ -54,89 +54,102 @@ module.exports = require('ridge/view').extend({
 		this._bindings = [];
 
 		_.each(this.bindings, function(opts, key) {
-			if(_.isString(opts))
-				opts = {
-					hook: key,
-					type: opts
-				};
+			function setupBinding(opts) {
+				if(_.isString(opts))
+					opts = {
+						hook: key,
+						type: opts
+					};
 
-			if(opts.hook) opts.selector = '[data-hook="' + opts.hook + '"]';
+				if(opts.hook) opts.selector = '[data-hook="' + opts.hook + '"]';
 
-			if(opts.type) opts.get = opts.set = opts.type;
+				if(opts.type) opts.get = opts.set = opts.type;
 
-			if(!_.isArray(opts.set)) opts.set = [ opts.set ];
+				if(!_.isArray(opts.set)) opts.set = [ opts.set ];
 
-			var $el = _form.$(opts.selector);
+				var $el = _form.$(opts.selector);
 
-			if($el.length === 0) return;
+				if($el.length === 0) return;
 
-			var namespace = key.split('.'),
-				getter = _getters[opts.get];
-			
-			var setter;
+				var namespace = key.split('.'),
+					getter = _getters[opts.get];
+				
+				// is object so we can set setter.disabled without having to check
+				var setter = {};
 
-			if(opts.set) {
-				setter = _setter.apply(null, opts.set.map(function(name) {
-					return _setters[name];
-				}));
+				if(opts.set) {
+					setter = _setter.apply(null, opts.set.map(function(name) {
+						return _setters[name];
+					}));
 
-				_form.listenTo(_form.model, 'change:' + namespace[0], function(model, value, opts) {
-					if(opts && opts.internalUpdate) return;
+					_form.listenTo(_form.model, 'change:' + namespace[0], function(model, value, opts) {
+						//if(opts && opts.internalUpdate) return;
+						if(setter.disabled) return;
 
-					for(var i = 0, ref = model.changedAttributes(); ref && i < namespace.length; i++) {
-						ref = ref[namespace[i]];
-					}
+						for(var i = 0, ref = model.changedAttributes(); ref && i < namespace.length; i++) {
+							ref = ref[namespace[i]];
+						}
 
-					setter($el, ref != null ? ref : null);
+						setter($el, ref != null ? ref : null);
+					});
+				}
+
+				if(getter) {
+					$el.on(getter.events.join(' '), function() {
+						var value = getter($el);
+
+						setter.disabled = true;
+
+						if(namespace.length > 1) {
+							// if the we have namespace of more than one level we
+							// need to clone the object, and iterate until we
+							// find the right property to set
+							var obj = _.clone(_form.model.get(namespace[0])) || {};
+
+							for(var i = 1, ref = obj; i < namespace.length - 1; i++) {
+								if(_.isObject(ref[namespace[i]])) ref[namespace[i]] = _.clone(ref[namespace[i]]);
+								else if(!ref[namespace[i]]) ref[namespace[i]] = {};
+
+								ref = ref[namespace[i]];
+							}
+
+							// remove or set the property depending on if value is set
+							if(value != null) {
+								if(!ref[namespace[i]]) ref[namespace[i]] = {};
+
+								ref[namespace[i]] = value;
+							} else
+								delete ref[namespace[i]];
+
+							// TODO perhaps unset if obj is empty element
+							if(_.isEmpty(obj))
+								_form.model.unset(namespace[0], { internalUpdate: true });
+							else
+								_form.model.set(namespace[0], obj, { internalUpdate: true });
+						} else {
+							// single level namespace, simply set or unset the model attribute
+							// depending if value is set
+							if(value != null)
+								_form.model.set(namespace[0], value, { internalUpdate: true });
+							else
+								_form.model.unset(namespace[0], { internalUpdate: true });
+						}
+
+						delete setter.disabled;
+					});
+				}
+
+				_form._bindings.push({
+					$el: $el,
+					el: $el[0],
+					getter: getter,
+					setter: setter,
+					namespace: key.split('.')
 				});
 			}
 
-			$el.on(getter.events.join(' '), function() {
-				var value = getter($el);
-
-				if(namespace.length > 1) {
-					// if the we have namespace of more than one level we
-					// need to clone the object, and iterate until we
-					// find the right property to set
-					var obj = _.clone(_form.model.get(namespace[0])) || {};
-
-					for(var i = 1, ref = obj; i < namespace.length - 1; i++) {
-						if(_.isObject(ref[namespace[i]])) ref[namespace[i]] = _.clone(ref[namespace[i]]);
-						else if(!ref[namespace[i]]) ref[namespace[i]] = {};
-
-						ref = ref[namespace[i]];
-					}
-
-					// remove or set the property depending on if value is set
-					if(value != null) {
-						if(!ref[namespace[i]]) ref[namespace[i]] = {};
-
-						ref[namespace[i]] = value;
-					} else
-						delete ref[namespace[i]];
-
-					// TODO perhaps unset if obj is empty element
-					if(_.isEmpty(obj))
-						_form.model.unset(namespace[0], { internalUpdate: true });
-					else
-						_form.model.set(namespace[0], obj, { internalUpdate: true });
-				} else {
-					// single level namespace, simply set or unset the model attribute
-					// depending if value is set
-					if(value != null)
-						_form.model.set(namespace[0], value, { internalUpdate: true });
-					else
-						_form.model.unset(namespace[0], { internalUpdate: true });
-				}
-			});
-
-			_form._bindings.push({
-				$el: $el,
-				el: $el[0],
-				getter: getter,
-				setter: setter,
-				namespace: key.split('.')
-			});
+			if(_.isArray(opts)) opts.forEach(setupBinding);
+			else setupBinding(opts);
 		});
 	},
 
