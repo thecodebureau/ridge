9857adf3bb0ae1f67f3929d88eac9f842d2d7beb
Linus Miller <lohfu@lohfu.io>
2015-10-06 19:37:52 +0200

    Cleared away old filtering, pagination and alike code from collection.

--- collection.js
+++ collection.js
@@ -1,36 +1,24 @@
-module.exports = Backbone.Collection.extend({
-	modelClasses: require('./ridge').extend({
-		models: {
-			Model: require('./model')
-		}
-	}).models,
+var app = require('ridge');
 
+module.exports = Backbone.Collection.extend({
 	constructor: function(attributes, options) {
 		var _collection = this;
 
 		Backbone.Collection.apply(_collection, arguments);
 
 		if(_.isString(_collection.model)) {
-			if(_collection.modelClasses[_collection.model])
-				_collection.model = _collection.modelClasses[_collection.model];
+			_collection.modelName = _collection.model;
+			if(app.models[_collection.model])
+				_collection.model = app.models[_collection.model];
 			else {
-				_collection.model = _collection.modelClasses.Model.extend({
+				_collection.model = require('ridge/model').extend({
 					name: _collection.model
 				});
 			}
 		}
 
-		if(options && options.defaultFilter)
-			_collection.defaultFilter = options.defaultFilter;
-
-		_collection.filter = _.clone(_collection.defaultFilter);
-
-		_collection.modelName = _collection.model.prototype.name;
-
 		return _collection;
 	},
-
-	defaultFilter: {},
 
 	reset: function (models, options) {
 		var _collection = this;
@@ -47,37 +35,8 @@ module.exports = Backbone.Collection.extend({
 
 		models = _collection.add(models, _.extend({silent: true}, options));
 
-		_collection.totalCount = parseInt(options.xhr.getResponseHeader('X-Collection-Length'));
-
 		if (!options.silent) _collection.trigger('reset', _collection, options);
 
 		return models;
-	},
-	
-	fetch: function(options) {
-		var _collection = this;
-
-		options = options || {};
-
-		if(_collection.filter)
-			options = _.extend(options || {}, { data: _collection.filter, processData: true });
-
-		Backbone.Collection.prototype.fetch.call(_collection, options);
-	},
-
-	setPage: function(page) {
-		if(page > 1)
-			this.filter._skip = (page - 1) * (this.filter._limit || this.length);
-		else
-			delete this.filter._skip;
-	},
-
-	setFilter: function(filter) {
-		if(filter)
-			this.filter = filter;
-		else
-			this.filter = _.clone(this.defaultFilter);
-	},
-
-
+	}
 });
