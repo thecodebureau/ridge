52263fc2bfe6bdcf3a5fed5eb8805c70264ce32b
Linus Miller <lohfu@lohfu.io>
2015-11-16 16:17:04 +0100

    el > , and trigger change html setter.

--- setters.js
+++ setters.js
@@ -1,11 +1,11 @@
 module.exports = {
-	html: function(el, value, previousValue) {
-		$(el).html(value);
+	html: function($el, value, previousValue) {
+		$el.html(value);
+
+		$el.trigger('change', { internalUpdate: true });
 	},
 
-	value: function(el, value, previousValue) {
-		var $el = $(el);
-
+	value: function($el, value, previousValue) {
 		if($el[0].type === 'radio' || $el[0].type === 'checkbox') {
 			if($el.length === 1) {
 				$el.prop('checked', !!value);
@@ -20,37 +20,37 @@ module.exports = {
 		} else {
 			if(value != null) value = value.toString();
 
-			$(el).val(value);
+			$el.val(value);
 		}
 
 		$el.trigger('change', { internalUpdate: true });
 	},
 
-	published: function(el, value) {
-		$el = $(this).closest('[data-published]');
+	published: function($el, value) {
+		$el = $el.closest('[data-published]');
 
 		$el.attr('data-published', (!!value).toString());
 	},
 
-	parts: function(el, value) {
+	parts: function($el, value) {
 		if(!_.isDate(value)) ref = new Date(value);
 
 		ref = ref.toLocaleString('se-SV').split(' ');
 
-		$(el).find('[data-part]').toArray().forEach(function(part, index) {
+		$el.find('[data-part]').toArray().forEach(function(part, index) {
 			$(part).val(ref[index]);
 		});
 	},
 
-	src: function(el, value) {
-		$(el).attr('src', value);
+	src: function($el, value) {
+		$el.attr('src', value);
 	},
 
-	selectMultiple: function(el, value) {
-		if(!value) $(el).val(null);
+	selectMultiple: function($el, value) {
+		if(!value) $el.val(null);
 		else
 			value.forEach(function(value) {
-				$(el).find('[value="' + value + '"]').prop('selected', true);
+				$el.find('[value="' + value + '"]').prop('selected', true);
 			});
 	}
 };
