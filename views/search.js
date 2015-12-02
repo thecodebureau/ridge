0e22f371cc500a9debe9a1b11b0f422bd3d78d74
Linus Miller <linus.miller@thecodebureau.com>
2015-12-02 12:52:37 +0100

    Search form now extends form view.

--- views/search.js
+++ views/search.js
@@ -1,51 +1,57 @@
-var app = require('ridge');
 
-module.exports = require('ridge/view').extend({
-	tagName: 'form',
-
-	events: {
-		'submit': function(e) {
-			e.preventDefault();
-
-			var query = [];
-			_.each(e.currentTarget.elements, function(elem) {
-				var value = elem.value,
-					name = elem.name;
-
-				if($(elem).is('[data-dot]')) {
-					// <select name="status" data-dot><option value="paid,false">Unpaid</option></select>
-					var arr = value.split(',');
-					name = name + '.' + arr[0];
-					value = arr[1];
-				} else if($(elem).is('[data-regex]'))
-					value = '/' + value + '/' + $(elem).attr('data-regex');
-
-				if (elem.name && elem.value)
-					query.push(encodeURIComponent(name) + '=' + encodeURIComponent(value).replace('%20', '+'));
-			});
-
-			var url = '/' + Backbone.history.fragment.split('?')[0];
-
-			if (query.length) url += '?' + query.join('&');
-
-			Backbone.history.navigate(url, { trigger: true });
-		},
-
-		'change .query-options': function() {
-			this.$('form').submit();
-		}
+var _filters = {
+	dot: function(value, key) {
+		// <select name="status" data-dot><option value="paid,false">Unpaid</option></select>
+		var arr = value.split(',');
+		name = name + '.' + arr[0];
+		value = arr[1];
 	},
 
-	initialize: function(opts) {
-		this.listenTo(app.router.current(), 'change:query', this.attach);
+	regex: function(flags) {
+		return function(value, key) {
+			return '/' + value + '/' + flags;
+		};
+	}
+};
+
+var app = require('ridge');
+
+var FormView = require('ridge/views/form');
+
+module.exports = FormView.extend({
+	submit: function(e) {
+		e.preventDefault();
+
+		var query = [];
+
+		_.each(this.$el.prop('elements'), function(elem) {
+			if (elem.name && elem.value)
+				query.push(encodeURIComponent(elem.name) + '=' + encodeURIComponent(elem.value).replace('%20', '+'));
+		});
+
+		var url = '/' + Backbone.history.fragment.split('?')[0];
+
+		if (query.length) url += '?' + query.join('&');
+
+		Backbone.history.navigate(url, { trigger: true });
+	},
+
+	initialize: function() {
+		this.listenTo(app.router.current(), 'change:query', this.setQuery);
+
+		this.delegate('submit', this.submit.bind(this));
 	},
 
 	attach: function() {
+		this.setQuery();
+	},
+
+	setQuery: function() {
 		var params = app.router.params;
 
-		_.each(this.$('form').prop('elements'), function(elem) {
+		_.each(this.$el.prop('elements'), function(elem) {
 			if (elem.name)
-				$(elem).val(params[elem.name]);
+				$(elem).val(params[elem.name]).trigger('change');
 		});
 	}
 });
