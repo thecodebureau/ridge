6fcc95227f3b18464ef3c6a6c233f81e89dc1b45
Victor Nilsson <victor.nilsson@thecodebureau.com>
2015-12-30 17:40:23 +0100

    Revert "Created onRoute method"
    This reverts commit 5ba8bd81d402c0e9ea18a928d458ee25338cb369.

--- ridge.js
+++ ridge.js
@@ -1,10 +1,10 @@
 require('./util/dust-mod');
 
 var dust = require('dustjs-linkedin');
-
 var Router = require('./router');
 
 var app = module.exports = _.create(Backbone.View.prototype, {
+
 	dust: dust,
 
 	helpers: dust.helpers,
@@ -47,81 +47,39 @@ var app = module.exports = _.create(Backbone.View.prototype, {
 		app.router = new Router({
 			routes: this.routes
 		});
-
-		app.router.states.reset(options.states, { parse: true });
-
-		Backbone.history.on('route', app.onRoute);
+		
+		app.router.states.reset(INITIAL_STATE, { parse: true });
 
 		Backbone.history.start(options);
 
 		// prevent scrolling on popState with { scrollRestoration: 'manual' }
-		if(window.history.scrollRestoration)
-			_.extend(window.history, _.pick(options, 'scrollRestoration'));
+		_.extend(window.history, _.pick(options, 'scrollRestoration'));
 
-		Backbone.View.call(app);
-	},
-
-	onRoute: function(router, name) {
-		var previous = router.states.current,
-			state = router.load();
-
-		if (previous && state === previous) return;
-
-		var options = _.extend({
-			state: state,
-			router: router,
-			name: name
-		}, router.options);
-
-		var done = app.switchPage.bind(this, options);
-
-		if (state.loading)
-			state.loading.done(done);
-		else
-			done();
+		$(function() {
+			Backbone.View.call(app);
+		});
 	},
 
 	createPage: function(options) {
 		var view = options && options.view;
-
 		if (!_.isFunction(view)) view = app.views[view || 'Page'];
 
 		return app.currentPage = new view(options);
 	},
 
 	switchPage: function(options) {
-		//var index = app.navigation.render().index();
-
-		// set animation class
-		//options.className = 
-		//	index > navIndex ? 'forward' :
-		//	index < navIndex ? 'back' :
-		//	'';
-
-		//navIndex = index;
+		window.scrollTo(0, 0);
 
 		if(app.currentPage) {
-			if(window.history.scrollRestoration === 'manual') {
-				window.scrollTo(0, 0);
-
-				_.result(app.currentPage, 'unscroll');
-			}
-
 			app.currentPage.leave(options);
+			app.createPage(options).enter(app.elements.main, options);
 		} else {
-			var $el = app.elements.main.children();
-
-			if($el.length) options.el = $el;
+			app.createPage(_.extend({
+				el: app.elements.main.children()
+			}, options));
 		}
 
-		var page = app.createPage(options);
-
-		if(!page.el.parentNode)
-			page.enter(app.elements.main, options);
-
-		document.body.className = options.state.get('page').name;
-
-		document.title = _.result(page, 'title', document.title);
+		document.title = _.result(app.currentPage, 'title', document.title);
 	},
 
 	remember: function(state) {
